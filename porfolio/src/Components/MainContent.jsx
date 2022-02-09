2263d7232a871aa13e27846d47c0603cd6ded098
GALHA <totogalha@gmail.com>
2022-02-09 19:25:00 -0300

    Add motion

--- porfolio/src/Components/MainContent.jsx
+++ porfolio/src/Components/MainContent.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import WebDevelopmentIMG from "../Img/webDevelopment.png";
 
 const MainContent = () => {
   return (
-    <div id="main-content" className="mt-20 flex">
+    <motion.div
+      id="main-content"
+      className="mt-20 flex"
+      initial={{ opacity: 0, translateX: 100 }}
+      animate={{ opacity: 1, translateX: 0 }}
+      transition={{ duration: 1.3 }}
+    >
       <img
         src={WebDevelopmentIMG}
         className="w-2/5 ml-20 mb-10 bg-gradient-to-r from-violet-500 to-fuchsia-500  rounded-full"
       />
-
-      <div id="main-text" className="ml-40 mt-14 w-1/3 ">
-        <h1 className="text-6xl text-transparent  border-2 pt-5 pb-5 pl-5 pm-5  bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500">
+      <div id="main-text" className="ml-20 mt-20 w-2/3">
+        <h1 className="text-7xl text-transparent  pt-5 pb-5 pl-5 pm-5  bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500">
           Web developer, Web artist
         </h1>
 
-        <p className="text-xl mt-20 text-gray-400  border-2 pt-5 pb-5 pl-5 pm-5 ">
-          Im Galha, glad to met you Welcome to my little section of the
+        <p className="text-xl w-2/3 mx-auto  mt-36 text-gray-400  border-t border-gray-200 pt-5 pb-5 pl-5 pm-5 ">
+          Im Galha, glad to met you. Welcome to my little section of the
           internet!
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- porfolio/src/Components/Pages/LandPage.jsx
+++ porfolio/src/Components/Pages/LandPage.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import Header from "../Header";
 import MainContent from "../MainContent";
 
 const LandPage = () => {
   return (
-    <div className="bg-black mt-32  w-3/4 mx-auto text-white">
+    <motion.div
+      initial={{ opacity: 0, translateX: -50 }}
+      animate={{ opacity: 1, translateX: 0 }}
+      transition={{ duration: 1.3 }}
+      className="bg-black  mt-20  w-5/6 mx-auto text-white  border  border-gray-200"
+    >
       {/*  Header */}
       <Header />
       {/* Main content */}
       <MainContent />
-    </div>
+    </motion.div>
   );
 };
 
